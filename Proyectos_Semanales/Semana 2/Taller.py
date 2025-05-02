93628d78dc84ca8abb5ceb1b40905c0c77345459
Jrestrepo18 <jerorrpo@gmail.com>
2025-05-02 16:31:05 -0500

    TERMINADO

--- Proyectos_Semanales/Semana 2/Taller.py
+++ Proyectos_Semanales/Semana 2/Taller.py
@@ -8,28 +8,36 @@
 #60 a 69: Aprobado
 #0 a 59: Reprobado
 
-calificacion = float(input("Ingrese la calificacion del estudiante: "))
-if calificacion <= 59:
-    print("Reprobado")
-elif calificacion >= 60 and calificacion <= 69:
-    print("Aprobado")
-elif calificacion >= 70 and calificacion <= 79:
-    print("Bueno")
-elif calificacion >= 80 and calificacion <= 89:
-    print("Muy bueno")
-else:
-    print("Excelente")
+RESET = '\033[0m'  
+GREEN = '\033[32m'  
+YELLOW = '\033[33m'  
+BLUE = '\033[34m' 
+RED = '\033[31m'  
+MAGENTA = '\033[35m' 
 
+
+calificacion = float(input("\nIngrese la calificación del estudiante: "))
+
+if calificacion <= 59:
+    print(f"{RED}Reprobado{RESET}")
+elif calificacion >= 60 and calificacion <= 69:
+    print(f"{YELLOW}Aprobado{RESET}")
+elif calificacion >= 70 and calificacion <= 79:
+    print(f"{BLUE}Bueno{RESET}")
+elif calificacion >= 80 and calificacion <= 89:
+    print(f"{MAGENTA}Muy bueno{RESET}")
+else:
+    print(f"{GREEN}Excelente{RESET}")
 
 # 2. Calcular el promedio:
 # Permitir al usuario ingresar una lista de calificaciones (separadas por comas)
 # Calcular y mostrar el promedio de las calificaciones en la lista.
-notas=[]
+notas_planas=[]
 
-calificaciones = input("Ingrese las calificaciones separadas por comas: ")
+calificaciones = input("\nIngrese las calificaciones separadas por comas: ")
 calificaciones = calificaciones.split(",")
 
-notas_planas = [int(nota.strip()) for nota in calificaciones]
+notas_planas = [float(nota.strip()) for nota in calificaciones]
 
 promedio = sum(notas_planas) / len(notas_planas)
 print(f"El promedio es: {promedio:.2f}")
@@ -39,7 +47,7 @@ print(f"El promedio es: {promedio:.2f}")
 # Preguntar al usuario por un valor específico
 # Contar cuántas calificaciones en la lista son mayores que este valor
 
-valor = int(input("Ingrese una nota (0 - 100) para comparar: "))
+valor = float(input("\nIngrese una nota (0 - 100) para comparar: "))
 
 contador = 0
 for nota in notas_planas:
@@ -47,3 +55,26 @@ for nota in notas_planas:
         contador += 1
 
 print(f"Hay {contador} calificaciones mayores que {valor}.")
+
+# Verificar y contar calificaciones específicas
+# Preguntar al usuario por una calificación específica. 
+# Verificar si esta calificación está en la lista y contar cuántas veces aparece, utilizando break y continue para controlar el flujo del programa.
+
+
+verificar=float(input("\nIngrese una calificacion: "))
+c = 0
+
+for i in notas_planas:
+    if i != verificar:
+        continue
+
+    c += 1
+
+if contador > 0:
+    print(f"la calificacion {verificar} aparece en el contador {c} veces")
+else:
+    print(f"\n\033[31mLa calificación {verificar} no aparece en la lista\033[0m")
+
+
+
+
