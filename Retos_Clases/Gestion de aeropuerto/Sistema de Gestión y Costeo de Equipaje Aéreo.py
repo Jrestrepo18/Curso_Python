9acbc174a7072c48ca942cc65b6166e382f3245b
Jrestrepo18 <jerorrpo@gmail.com>
2025-05-02 21:25:31 -0500

    Inicoo

--- Retos_Clases/Gestion de aeropuerto/Sistema de Gestión y Costeo de Equipaje Aéreo.py
+++ Retos_Clases/Gestion de aeropuerto/Sistema de Gestión y Costeo de Equipaje Aéreo.py
@@ -4,12 +4,44 @@ id_usurio = 1
 
 print("\n-------------------- Bienvenido al Sistema de Gestión y Costeo de Equipaje Aéreo ✈️--------------------\n")
 nombre_usurio = input("Ingrese su nombre: ")
-tipo_viaje = input("Tipo de viaje 1) nacional / 2) internacional: ")
-peso_equipaje = float(input("Peso de su equipaje: "))
-equipaje_mano = input("¿Lleva equipaje de mano S(si) - N(no)")
-fecha_viaje = input("Fecha del viaje: ")
+tipo_viaje = input("\nTipo de viaje 1) nacional / 2) internacional: ")
+if tipo_viaje == "nacional":
+    costo_base= 240000
+    print("\nCosto del viaje 230.000.")
+elif tipo_viaje == "internacional":
+    costo_base= 4200000
+    print("\nCosto del viaje 4.200.000.")
+else:
+    print("\nViaje no disponible.")
 
-op = input("\Ingrese una opcion: ")
+peso_equipaje = float(input("Peso de su equipaje: "))
+if peso_equipaje <= 20:
+    print("\nValor a pagar es de $50.000")
+    costo_equipaje = 50.000
+elif peso_equipaje <= 30:
+    print("\nValor a pagar es de $70.000")
+    costo_equipaje = 70.000
+elif peso_equipaje < 49:
+    print("\nValor a pagar es de $110.000")
+    peso_equipaje =110.000
+else:
+    print("\nNo admitido, debe cancelar o viajar sin equipaje.")
+
+equipaje_mano = input("¿Lleva equipaje de mano S(si) - N(no)").lower()
+if equipaje_mano == "s":
+    max_permitido = input("Peso equipaje de mano: ")
+    if max_permitido <= 13:
+        print("Equipaje de mano permitido.")
+    else:
+        print("Equipaje de mano permitido, puede abordar el avion sin equipaje de mano.")
+elif equipaje_mano == "n":
+    print("Sin equipaje de mano.")
+else:
+    print("Respuesta no permitida")
+
+total_pajar = costo_base + costo_equipaje
+
+fecha_viaje = input("Fecha del viaje: ")
 
 usurios[id_usurio] = {
     "nombre":nombre_usurio,
@@ -20,4 +52,3 @@ usurios[id_usurio] = {
 
 }
 
-usurios
