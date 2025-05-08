28d1d3df0dffa772d921794aeb0d23a8bf274314
Jrestrepo18 <jerorrpo@gmail.com>
2025-05-08 01:12:52 -0500

    Borre anotaciones

--- Proyectos_Semanales/Semana 3/index.py
+++ Proyectos_Semanales/Semana 3/index.py
@@ -1,22 +1,7 @@
-#Añadir productos:
-#Producto definido por nombre, precio y cantidad.
-#crecer dinamicamente.
-
-#consultar productor:
-#Buscar producto por nombre y obtener detalles precio y cantidad disponible.
-#Siel producto no esta, se debe hacer validacion.abs
-
-#Eiminar productos
-#ELiminar productos de forma segura.abs
-
 #Calcular el valor toatal del inventario.
 #Valor total de los productos en el inventario.
 #Utilizar una funcion anonima lambda para el calculo.abs
 
-#Almacenar productos en un diccinario
-#EL nombre debe ser la clave y la cantidad y el precio deben ser los valores
-
-#Debe ser interactivo.
 from funciones import *
 print("\n\033[33m---------- Sistema de gestión de inventarios ----------\033[0m\n")
 while True:
